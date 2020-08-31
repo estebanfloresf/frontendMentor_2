aedaa5b421dc78e377f32b231575fd08f609235a
Esteban <esteban_fs@hotmail.com>
2020-08-30 22:33:14 -0500

    all

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
